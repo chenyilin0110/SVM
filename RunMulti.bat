fe3478949f952e21b1e8de205739cf403f99fe4d
chenyilin0110 <a0989735018@gmail.com>
2019-02-19 20:37:23 +0800

    change output

---
+++ RunMulti.bat
@@ -0,0 +1,14 @@
+set finder=multi
+set outputLayer=41
+set testing=20
+set iteration=4000
+
+del %finder%class.txt
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+for /L %%d in (1,1,15) do (
+set filename=data%%d.csv
+python main.py %finder% !filename! %outputLayer% %testing% %iteration% %%d >> %finder%class.txt
+)
+
+pause
---
+++ RunMulti.sh
@@ -0,0 +1,12 @@
+finder=multi
+outputLayer=41
+testing=20
+iteration=4000
+
+rm $finder"class.txt"
+
+for i in {1..15}
+do
+    filename="data"$i.csv
+    python3 main.py $finder $filename $outputLayer $testing $iteration $i >> $finder"class.txt"
+done
